c979916a6ade87792b0034046b789833d32b4ea9
Usman Ahmad <ua803408@gmail.com>
2022-02-18 05:44:11 +0400

    Second commit

---
+++ lessons.js
@@ -0,0 +1,110 @@
+let lessons = [
+    {
+        "id": 01,
+        "subject": 'Music',
+        "location": "London",
+        "price": 300,
+        "image": "img/music.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 02,
+        "subject": 'Art',
+        "location": "Paris",
+        "price": 250,
+        "image": "img/art.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 03,
+        "subject": "Physics",
+        "location": "Houston",
+        "price": 200,
+        "image": "img/physics.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 04,
+        "subject": "English",
+        "location": "Houston",
+        "price": 150,
+        "image": "img/english.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 05,
+        "subject": "Math",
+        "location": "Dubai",
+        "price": 200,
+        "image": "img/math.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 06,
+        "subject": 'Gymnastics',
+        "location": "London",
+        "price": 350,
+        "image": "img/gymn.jpg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 07,
+        "subject": 'Psychology',
+        "location": "Dubai",
+        "price": 170,
+        "image": "img/psychology.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 08,
+        "subject": 'Marketting',
+        "location": "Dubai",
+        "price": 240,
+        "image": "img/marketing.jpg",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 09,
+        "subject": 'Geography',
+        "location": "Houston",
+        "price": 190,
+        "image": "img/geography.jpeg",
+        "availableInventory":5,
+        "rating": 5
+    },
+    {
+        "id": 010,
+        "subject": 'Biology',
+        "location": "Paris",
+        "price": 200,
+        "image": "img/biology.jpg",
+        "availableInventory": 5,
+        "rating": 3,
+    },
+    {
+        "id": 011,
+        "subject": 'Economics',
+        "location": "Dubai",
+        "price": 300,
+        "image": "img/economics.png",
+        "availableInventory":5,
+        "rating": 4
+    },
+    {
+        "id": 012,
+        "subject": 'Accounting & Finance',
+        "location": "London",
+        "price": 260,
+        "image": "img/Accounting.jpg",
+        "availableInventory":5,
+        "rating": 5
+    }
+]
