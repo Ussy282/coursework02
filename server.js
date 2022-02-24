1c02c64a412e315d072ebefb3e79574c71027b13
Usman Ahmad <ua803408@gmail.com>
2022-02-24 20:38:08 +0400

    added cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(exp.json())
 const port = process.env.PORT || 3000
 app.use((req, res, next) => {
    
+    res.header('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
